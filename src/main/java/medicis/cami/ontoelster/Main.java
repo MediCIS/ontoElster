0fe4dc7f363b0ee3c549ba0436f93da28482da64
Javier Rojas Balderrama <javier.rojas-balderrama@univ-rennes1.fr>
2017-04-08 12:23:32 +0200

    separating businness code in several classes

--- src/main/java/medicis/cami/ontoelster/Main.java
+++ src/main/java/medicis/cami/ontoelster/Main.java
@@ -1,16 +1,10 @@
 package medicis.cami.ontoelster;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.OpenOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import static java.nio.file.StandardOpenOption.CREATE;
-import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
-import static java.nio.file.StandardOpenOption.WRITE;
-import java.util.ArrayList;
 import java.util.List;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.formats.RDFXMLDocumentFormat;
@@ -18,12 +12,9 @@ import org.semanticweb.owlapi.io.FileDocumentSource;
 import org.semanticweb.owlapi.io.FileDocumentTarget;
 import org.semanticweb.owlapi.io.OWLOntologyDocumentSource;
 import org.semanticweb.owlapi.io.OWLOntologyDocumentTarget;
-import org.semanticweb.owlapi.model.AddAxiom;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.MissingImportHandlingStrategy;
-import org.semanticweb.owlapi.model.OWLAnnotation;
 import org.semanticweb.owlapi.model.OWLDocumentFormat;
-import org.semanticweb.owlapi.model.OWLLiteral;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyLoaderConfiguration;
@@ -32,131 +23,49 @@ import org.semanticweb.owlapi.model.OWLOntologyStorageException;
 
 /**
  * This program filters selected classes from an existing Ontology, saving those
- * in a new (Sub)Ontology File and save them in a CVS file. Created by Chantal
- * and Javier on 4/6/2017.
+ * in a new (Sub)Ontology File and save them in a CVS file.
+ * 
+ *  @author chantal
  */
 public class Main {
-
-    public static IRI DEFINITION = IRI.create("http://purl.obolibrary.org/obo/IAO_0000115");
-    public static IRI ALT_LABEL = IRI.create("http://www.w3.org/2004/02/skos/core#altLabel");
-    public static IRI PREF_LABEL = IRI.create("http://www.w3.org/2004/02/skos/core#prefLabel");
 
     public static void main(String... arguments)
             throws OWLOntologyCreationException, IOException, OWLOntologyStorageException {
 
         OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-        // Load Ontology from Web
-        IRI iri = IRI.create("https://medicis.univ-rennes1.fr/ontologies/ontospm/OntoSPM.owl");
+        // // Load ontology from Web
+        // IRI iri = IRI.create("https://medicis.univ-rennes1.fr/ontologies/ontospm/OntoSPM.owl");
         // OWLOntology ontology = manager.loadOntologyFromOntologyDocument(iri);
-        // Load Ontology from local File
+
+        // Load ontology from local file
         File ontologyFile = new File("C:\\Users\\medicis\\Documents\\Data\\OWLs\\151112_OntoSPM_RDF2.owl");
         OWLOntologyDocumentSource source = new FileDocumentSource(ontologyFile);
+
+        // Load ontology ignoring imports 
         OWLOntologyLoaderConfiguration configuration = new OWLOntologyLoaderConfiguration();
-        // Ignore Imports from Ontology
         configuration = configuration.setMissingImportHandlingStrategy(MissingImportHandlingStrategy.SILENT);
         OWLOntology ontology = manager.loadOntologyFromOntologyDocument(source, configuration);
-        IRI excerptIRI = IRI.create("https://medicis.univ-rennes1.fr/ontologies/ontospm/subOntoSPM.owl");
-        OWLOntology excerptOntology = manager.createOntology(excerptIRI);
 
-        // Read File with selected classes
+        // Read file with selected classes
         String selectionFileName = "C:\\Users\\medicis\\IdeaProjects\\ontoElster\\src\\main\\java\\selection.txt";
         List<String> selectedClasses = Files.readAllLines(Paths.get(selectionFileName));
 
-        // Get all declared classes of the ontology
-        ontology.classesInSignature()
-                // Filter selected classes from the Ontology
-                .filter(c -> selectedClasses.contains(c.getIRI().toURI().getFragment()))
-                .forEach(c -> {
+        // Get a subontology based on selected classes from the reference ontology
+        OntologyExtractor extractor = new OntologyExtractor(manager);
+        IRI subontologyIRI = IRI.create("https://medicis.univ-rennes1.fr/ontologies/ontospm/subOntoSPM.owl");
+        OWLOntology subontology = extractor.getSubontology(ontology, subontologyIRI, selectedClasses);
 
-                    // Get subclasses of current ontology class and add to the excerpt ontology
-                    ontology.subClassAxiomsForSubClass(c)
-                    .forEach(sc -> {
-                        AddAxiom addAxiom = new AddAxiom(excerptOntology, sc);
-                        manager.applyChange(addAxiom);
-                    });
-
-                    //Get all annotations of the current class and add to the excerpt ontology
-                    ontology.annotationAssertionAxioms(c.getIRI())
-                    .forEach(ca -> {
-                        AddAxiom addAxiom = new AddAxiom(excerptOntology, ca);
-                        manager.applyChange(addAxiom);
-                    });
-                });
-
+        // Save the subontology into a file as RDF document
         File file = new File("C:\\Users\\medicis\\IdeaProjects\\ontoElster\\src\\main\\java\\excerpt.owl");
         OWLDocumentFormat format = new RDFXMLDocumentFormat();
         OWLOntologyDocumentTarget target = new FileDocumentTarget(file);
-        manager.saveOntology(excerptOntology, format, target);
+        manager.saveOntology(subontology, format, target);
 
         // Collection of all annotations by class
-        List<ClassAnnotations> annotations = new ArrayList<>();
-        // Get classes of SubOntology and writing them into a CVS file
-        excerptOntology.classesInSignature()
-                // filter classes by selection list get all fields of annotations
-                .filter(c -> selectedClasses.contains(c.getIRI().toURI().getFragment()))
-                .forEach(c -> {
+        List<ClassAnnotations> annotations = OntologyAnnotationsFormatter.annotations(subontology, selectedClasses);
 
-                    ClassAnnotations ann = new ClassAnnotations();
-                    ann.setUri(c.getIRI().toURI());
-                    excerptOntology.annotationAssertionAxioms(c.getIRI())
-                    .forEach(ca -> {
-
-                        OWLAnnotation annotation = ca.getAnnotation();
-                        IRI property = annotation.getProperty().getIRI();
-                        OWLLiteral value = annotation.getValue().asLiteral().get();
-
-                        // Get labels by language
-                        if (property.equals(PREF_LABEL)) {
-                            String literal = value.getLiteral();
-                            String language = value.getLang();
-                            switch (language) {
-                                case "fr":
-                                    ann.setFr(literal);
-                                    break;
-                                case "de":
-                                    ann.setDe(literal);
-                                    break;
-                                case "en":
-                                    ann.setEn(literal);
-                                    break;
-                            }
-                        }
-
-                        // Get alternative labels by language
-                        if (property.equals(ALT_LABEL)) {
-                            String literal = value.getLiteral();
-                            String language = value.getLang();
-                            switch (language) {
-                                case "fr":
-                                    ann.setAltFr(literal);
-                                    break;
-                                case "de":
-                                    ann.setAltDe(literal);
-                                    break;
-                                case "en":
-                                    ann.setAltEn(literal);
-                                    break;
-                            }
-                        }
-
-                        // Get definition and reformatting text
-                        if (property.equals(DEFINITION)) {
-
-                            // Replacing new lines by spaces
-                            String literal = value.getLiteral().replaceAll("[\\t|\\r?\\n]+", " ");
-                            ann.setDefinition(literal);
-                        }
-                    });
-                    annotations.add(ann);
-                });
-
-        // Save collection of annotations into a CVS
+        // Save collection of annotations into a CSVs
         Path csv = Paths.get("C:\\Users\\medicis\\IdeaProjects\\ontoElster\\src\\main\\java\\excerpt.csv");
-        OpenOption[] options = new OpenOption[]{WRITE, CREATE, TRUNCATE_EXISTING};
-        try (BufferedWriter writer = Files.newBufferedWriter(csv, options)) {
-            for (ClassAnnotations annotation : annotations) {
-                writer.write(annotation.toString());
-            }
-        }
+        OntologyAnnotationsFormatter.print(csv, annotations);
     }
 }
---
+++ src/main/java/medicis/cami/ontoelster/OntologyAnnotationsFormatter.java
@@ -0,0 +1,121 @@
+package medicis.cami.ontoelster;
+
+import java.io.BufferedWriter;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import static java.nio.file.StandardOpenOption.CREATE;
+import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
+import static java.nio.file.StandardOpenOption.WRITE;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLAnnotation;
+import org.semanticweb.owlapi.model.OWLLiteral;
+import org.semanticweb.owlapi.model.OWLOntology;
+
+/**
+ * Get classes of ontology and writing them into a CVS file.
+ *
+ * @author javier
+ */
+public class OntologyAnnotationsFormatter {
+
+    public static IRI DEFINITION = IRI.create("http://purl.obolibrary.org/obo/IAO_0000115");
+    public static IRI ALT_LABEL = IRI.create("http://www.w3.org/2004/02/skos/core#altLabel");
+    public static IRI PREF_LABEL = IRI.create("http://www.w3.org/2004/02/skos/core#prefLabel");
+
+    /**
+     * Obtain definition (IAO_0000115) and labels (SKOS "prefLabel", and
+     * "altLabel") annotations in German, English, and French.
+     *
+     * @param ontology Reference from which annotations are extracted
+     * @param classes List of classes into the ontology to get annotations
+     * @return A list of ClassAnnotations of filtered classes
+     */
+    public static List<ClassAnnotations> annotations(final OWLOntology ontology, final List<String> classes) {
+
+        return ontology.classesInSignature()
+                // filter classes by selection list get all fields of annotations
+                .filter(c -> classes.contains(c.getIRI().toURI().getFragment()))
+                .map(c -> {
+
+                    ClassAnnotations classAnnotations = new ClassAnnotations();
+                    classAnnotations.uri = c.getIRI().toURI();
+                    ontology.annotationAssertionAxioms(c.getIRI())
+                    .forEach(ca -> {
+
+                        OWLAnnotation annotation = ca.getAnnotation();
+                        IRI property = annotation.getProperty().getIRI();
+                        OWLLiteral value = annotation.getValue().asLiteral().get();
+                        String literal = value.getLiteral();
+
+                        // Get labels by language
+                        if (property.equals(PREF_LABEL)) {
+
+                            String language = value.getLang();
+                            switch (language) {
+                                case "fr":
+                                    classAnnotations.fr = literal;
+                                    break;
+                                case "de":
+                                    classAnnotations.de = literal;
+                                    break;
+                                case "en":
+                                    classAnnotations.en = literal;
+                                    break;
+                            }
+                        }
+
+                        // Get alternative labels by language
+                        if (property.equals(ALT_LABEL)) {
+
+                            String language = value.getLang();
+                            switch (language) {
+                                case "fr":
+                                    classAnnotations.altFr = literal;
+                                    break;
+                                case "de":
+                                    classAnnotations.altDe = literal;
+                                    break;
+                                case "en":
+                                    classAnnotations.altEn = literal;
+                                    break;
+                            }
+                        }
+
+                        // Get definition and reformatting text
+                        if (property.equals(DEFINITION)) {
+
+                            // Replacing new lines by spaces
+                            literal = literal.replaceAll("[\\t|\\r?\\n]+", " ");
+                            classAnnotations.definition = literal;
+                        }
+                    });
+
+                    return classAnnotations;
+                })
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Print a collection of ClassAnnotations in to a file.
+     *
+     * @param file Path to a file system file
+     * @param annotations List of ClassAnnotations to print
+     * @throws java.io.IOException
+     */
+    public static void print(final Path file, final List<ClassAnnotations> annotations)
+            throws
+            java.io.IOException {
+
+        OpenOption[] options = new OpenOption[]{WRITE, CREATE, TRUNCATE_EXISTING};
+        try (BufferedWriter writer = Files.newBufferedWriter(file, options)) {
+
+            for (ClassAnnotations annotation : annotations) {
+
+                writer.write(annotation.toString());
+            }
+        }
+    }
+}
